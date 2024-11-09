68b9ecfa8ef8cea3a7e6d846b3187b5249bdd262
Aaron Chacko <143901525+Aaron-Chacko@users.noreply.github.com>
2024-11-09 15:03:22 +0530

    Update selectclass.java

--- 3. Select Class/selectclass.java
+++ 3. Select Class/selectclass.java
@@ -1,1 +1,48 @@
+package com.aaron.tbav;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class cl_select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_classselect);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void selectknight(View view) {
+        Intent selectknightscr = new Intent(this, knight.class);
+        startActivity(selectknightscr);
+    }
+
+    public void selectwizard(View view){
+        Intent selectwizscr;
+        selectwizscr = new Intent(this, wizard.class);
+        startActivity(selectwizscr);
+    }
+
+    public void selectguardian(View view) {
+        Intent selectguardianscr = new Intent(this, guardian.class);
+        startActivity(selectguardianscr);
+    }
+
+    public void selectelf(View view) {
+        Intent selectelfscr = new Intent(this, elf.class);
+        startActivity(selectelfscr);
+    }
+}
 
